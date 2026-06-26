523e617794a9cf8dc793a7a25a7ef5ffd85080a3
seemlyx4 <santiagogr007@gmail.com>
2026-06-25 21:22:28 -0600

    main.py actualizado para InterfazV4

---
+++ main.py
@@ -0,0 +1,13 @@
+import flet as ft
+import os
+from interfaz import logo
+
+if __name__ == "__main__":
+    # Directorio donde se encuentra este archivo (para los assets)
+    carpeta_recursos = os.path.dirname(__file__)
+    
+    # Ejecutamos la aplicación con la función logo como punto de entrada
+    ft.app(target=logo, assets_dir=carpeta_recursos)
+
+    #actualmente este documento hace lo mismo que si se corriera interfaz.py, pero es necesario para 
+    # poder ejecutar la app desde un archivo .exe
